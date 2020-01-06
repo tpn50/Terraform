2df365fcdcd7a98ab6b2d579e0890e39086795f9
tpn50 <58995104+tpn50@users.noreply.github.com>
2020-01-06 19:19:23 +0530

    Var_Map

---
+++ ter.tf
@@ -0,0 +1,38 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "amis" {
+  type = "map"
+  default =  {
+    "ap-south-1" = "ami-0123b531fc646552f"
+    "us-east-1" = ""
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region = var.region
+}
+
+resource "aws_instance" "web" {
+  ami = var.amis[var.region]
+  instance_type = "t2.micro"
+  key_name = "key1"
+
+
+  connection {
+    user = "ubuntu"
+    private_key = file("key1.pem")
+    host = self.public_ip
+  }
+
+  
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt install docker.io -y",
+      "sudo docker pull tpn50/web_app:v1"
+    ]
+  }
+}
